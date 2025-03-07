d0e04842803333324e9a26efe9c6832f4638ad8e
ArtyomKoshurin <coshurin.art@yandex.ru>
2025-03-07 16:57:54 +0300

    Added status change

--- cafe_reservation/orders/serializers.py
+++ cafe_reservation/orders/serializers.py
@@ -47,9 +47,7 @@ class OrderCreateSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         items = validated_data.pop("items")
-        total_price = 0
-        for dish in items:
-            total_price += dish.get("price")
+        total_price = sum(dish["price"] for dish in items)
 
         order = Order.objects.create(
             table_number=validated_data.get("table_number"),
@@ -82,50 +80,47 @@ class OrderUpdateSerializer(serializers.ModelSerializer):
         fields = ("id", "table_number", "items", "total_price", "status")
 
     def validate(self, data):
-        items = data.get('items')
-        status = data.get("status")
-        if not items:
-            raise serializers.ValidationError(
-                "Необходимо добавить блюда в заказ."
-                )
-        for dish in items:
-            name = dish.get("name")
-            price = dish.get("price")
-            if not Dish.objects.filter(name=name, price=price).exists():
+        if "items" in data:
+            items = data.get('items')
+            if not items:
                 raise serializers.ValidationError(
-                    f"Блюда {name} с ценой {price} у нас в продаже нет."
+                    "Необходимо добавить блюда в заказ."
+                    )
+            for dish in items:
+                name = dish.get("name")
+                price = dish.get("price")
+                if not Dish.objects.filter(name=name, price=price).exists():
+                    raise serializers.ValidationError(
+                        f"Блюда {name} с ценой {price} у нас в продаже нет."
+                    )
+        if "status" in data:
+            status = data.get("status")
+            if not status or status not in ["Waiting", "Ready", "Paid"]:
+                raise serializers.ValidationError(
+                    "Укажите корректный статус для изменения заказа."
                 )
-        if status and status not in ["Waiting", "Ready", "Paid"]:
-            raise serializers.ValidationError("Неверный статус заказа.")
+
         return data
 
     def update(self, instance, validated_data):
-        table_number = validated_data.get(
-            "table_number",
-            instance.table_number
-        )
-        status = validated_data.get(
-            "status",
-            instance.status
-        )
-        items = validated_data.pop("items")
-        total_price = 0
-        for dish in items:
-            total_price += dish.get("price")
 
-        instance.items.clear()
-        for dish in items:
-            dish = Dish.objects.get(
-                name=dish.get("name"),
-                price=dish.get("price")
-            )
-            DishesForOrder.objects.create(dish=dish, order=instance)
+        if "items" in validated_data:
+            items = validated_data.pop("items")
+            total_price = sum(dish["price"] for dish in items)
 
-        instance.table_number = table_number
-        instance.total_price = total_price
-        instance.status = status
+            instance.items.clear()
+            for dish in items:
+                dish = Dish.objects.get(
+                    name=dish.get("name"),
+                    price=dish.get("price")
+                )
+                DishesForOrder.objects.create(dish=dish, order=instance)
+            instance.total_price = total_price
+
+        if "status" in validated_data:
+            instance.status = validated_data.get("status", instance.status)
+
         instance.save()
-
         return instance
 
 
